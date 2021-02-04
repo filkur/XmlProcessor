bb6c7e644f5de5a591b8a8da462ead8186f15dd1
filkur <filipkuranda@gmail.com>
2021-02-04 11:51:54 +0100

    Pares Java objects to XML

--- src/main/java/app/dataStream/JavaToXml.java
+++ src/main/java/app/dataStream/JavaToXml.java
@@ -1,4 +1,24 @@
 package app.dataStream;
 
+import app.model.Accounts;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
+
 public class JavaToXml {
+    public static void marshal(Accounts accounts){
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Accounts.class);
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+            jaxbMarshaller.marshal(accounts, new File("output.xml"));
+        } catch (JAXBException exception){
+            System.out.println(exception.getErrorCode()+exception.getMessage());
+        } catch (IllegalArgumentException exception){
+            System.out.println("Can't find the input file. make sure it exists");
+        }
+    }
 }

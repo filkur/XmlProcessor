cd8714c7964afa169b51f3cdbb98a56a0e83a38f
filkur <filipkuranda@gmail.com>
2021-02-04 11:46:32 +0100

    Class responsible for parse Xml to Java class

---
+++ src/main/java/app/dataStream/XmlToJava.java
@@ -0,0 +1,19 @@
+package app.dataStream;
+
+import app.model.Accounts;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class XmlToJava {
+    public static Accounts unmarshall () throws JAXBException, FileNotFoundException {
+        JAXBContext context = JAXBContext.newInstance(Accounts.class);
+        return (Accounts) context.createUnmarshaller()
+                .unmarshal(new FileReader("./input.xml"));
+    }
+}

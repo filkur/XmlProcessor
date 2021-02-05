61b239b874fd0200e93c445df0ad35e0add30efa
filkur <filipkuranda@gmail.com>
2021-02-05 16:30:30 +0100

    Add new architecture

--- src/main/java/app/filter/AccountFilter.java
+++ src/main/java/app/filter/AccountFilter.java
@@ -1,10 +1,46 @@
 package app.filter;
 
-public interface AccountFilter {
+import app.model.account.Account;
+import lombok.NoArgsConstructor;
+import nl.garvelink.iban.Modulo97;
 
-    boolean checkCurrency();
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
-    boolean checkBalance();
+@NoArgsConstructor
+public class AccountFilter implements AccountFilterRepository{
+    @Override
+    public List<Account> checkCurrency(List<Account> accountList) {
+        accountList.removeIf(account -> !account.getCurrency().equals("PLN"));
+        return accountList;
+    }
 
-    boolean checkClosingDate();
+    @Override
+    public List<Account> checkBalance(List<Account> accountList) {
+        accountList.removeIf(account -> account.getBalance()<0);
+        return accountList;
+    }
+
+    @Override
+    public List<Account> checkClosingDate(List<Account> accountList) {
+        accountList.removeIf(account -> {
+            try {
+                Date closing = new SimpleDateFormat("yyyy-MM-dd").parse(account.getClosingDate());
+                Date today = new Date();
+                return !closing.after(today);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return false;
+            }
+        });
+        return accountList;
+    }
+
+    @Override
+    public List<Account> checkIban(List<Account> accountList) {
+        accountList.removeIf(account -> Modulo97.verifyCheckDigits(account.getIban()));
+        return accountList;
+    }
 }

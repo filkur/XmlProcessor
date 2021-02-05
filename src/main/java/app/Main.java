40018b95ac08d8e93081e527330866a9ed3c8e56
filkur <filipkuranda@gmail.com>
2021-02-05 20:57:56 +0100

    Add logs

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,6 @@
 package app;
 
 import app.processor.Processor;
-
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 
--- src/main/java/app/processor/Processor.java
+++ src/main/java/app/processor/Processor.java
@@ -6,19 +6,20 @@ import app.model.Accounts;
 import app.filter.DataFilter;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.xml.bind.JAXBException;
 import java.io.FileNotFoundException;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Processor {
     public static void startProcessing() throws JAXBException, FileNotFoundException {
+        Logger logger = LoggerFactory.getLogger(Processor.class);
+        logger.info("Searching for a input file...");
         Accounts accounts = XmlToJava.unmarshall();
+        logger.info("File exist! Start processing...");
         JavaToXml.marshal(DataFilter.filter(accounts));
-
-        /*List<Account> accountList= accounts.getAccountsList();
-        for (Account account: accountList){
-            System.out.println(account);
-        }*/
-
+        logger.info("Processing Done!");
     }
 }
